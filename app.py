7fcd40d52da5a72b9e6bc6a15b45ab34eb975969
asam-anderson <and_asam@ad.ucla.edu>
2024-08-24 10:57:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def main():
     data = data_main['Adj Close'].T
     data_adjusted = data_main['Adj Close'].T
     ##Loading all stock data gonna take 3 mins to run 
-    
+    st.write(data)
     data = data.T[data.columns>=transactions[transactions.Action=='Buy'].Date.max()].T
     data_adjusted = data_adjusted.T[data_adjusted.columns>=transactions[transactions.Action=='Buy'].Date.max()].T
 
