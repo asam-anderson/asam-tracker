929ac425234a7f1f68f815ecdc0693b6a0417736
asam-anderson <and_asam@ad.ucla.edu>
2024-08-24 11:15:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def main():
 
     transactions['Date'] = pd.to_datetime(transactions['Date'])
     max_buy_date = transactions[transactions.Action == 'Buy']['Date'].max()
-    data = data.T[data.columns>=transactions[transactions.Action=='Buy'].Date.max()].T
+    data = data.T[data.columns >= max_buy_date].T
     data_adjusted = data_adjusted.T[data_adjusted.columns>=transactions[transactions.Action=='Buy'].Date.max()].T
 
     history = postions_calc[['Group','Tickers']].copy()
