ea3ba2c932c07dd32b579511659cc2a4d27d4b47
asam-anderson <and_asam@ad.ucla.edu>
2024-08-24 10:16:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import openpyxl
 import math
 from pandas_datareader import data as pdr
-import fix-yahoo-finance as yf
+import fix_yahoo_finance as yf
 from datetime import datetime, timedelta
 import warnings
 import statsmodels.api as sm
